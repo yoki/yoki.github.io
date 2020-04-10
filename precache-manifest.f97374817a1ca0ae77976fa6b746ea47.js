0274e53fa196b7881fa202219245e05147d2013d
Yoki <yotokyos+git@gmail.com>
2020-04-10 01:52:44 -0400

    https for api

---
+++ precache-manifest.f97374817a1ca0ae77976fa6b746ea47.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc7a63e4a5774659b2b",
+    "url": "/css/Terms.0ecdbae9.css"
+  },
+  {
+    "revision": "f18a2e27b0315d861501",
+    "url": "/css/Terms~courses~login~questions.4e4a1559.css"
+  },
+  {
+    "revision": "8f7632472db6631750d5",
+    "url": "/css/app.f3acc9aa.css"
+  },
+  {
+    "revision": "260b23e4c92217573202",
+    "url": "/css/chunk-vendors.90e4143e.css"
+  },
+  {
+    "revision": "76e9555508f29967c2a4",
+    "url": "/css/courses.f2d89637.css"
+  },
+  {
+    "revision": "9e98bfa3c90f1ba06e79",
+    "url": "/css/courses~login~questions.f2ed16dc.css"
+  },
+  {
+    "revision": "e769066e1a4de2371480",
+    "url": "/css/login.f825a3c6.css"
+  },
+  {
+    "revision": "4cc0adfe38013f4cdc43",
+    "url": "/css/questions.d28120b7.css"
+  },
+  {
+    "revision": "d355656cc304edc728ac1245a955784e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cc7a63e4a5774659b2b",
+    "url": "/js/Terms.548714b7.js"
+  },
+  {
+    "revision": "f18a2e27b0315d861501",
+    "url": "/js/Terms~courses~login~questions.704a5dc5.js"
+  },
+  {
+    "revision": "8f7632472db6631750d5",
+    "url": "/js/app.14e7a92c.js"
+  },
+  {
+    "revision": "260b23e4c92217573202",
+    "url": "/js/chunk-vendors.b996a3f1.js"
+  },
+  {
+    "revision": "76e9555508f29967c2a4",
+    "url": "/js/courses.014e7d54.js"
+  },
+  {
+    "revision": "9e98bfa3c90f1ba06e79",
+    "url": "/js/courses~login~questions.98de8081.js"
+  },
+  {
+    "revision": "e769066e1a4de2371480",
+    "url": "/js/login.cf57d3d4.js"
+  },
+  {
+    "revision": "4cc0adfe38013f4cdc43",
+    "url": "/js/questions.976017fe.js"
+  },
+  {
+    "revision": "4133de22dfe345231b3e5515f18d81c1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
